e42f945340793f8fd61bed9ace5c9c0d68214197
Yusuf Wadi <yusufwadi1@gmail.com>
2022-09-07 08:41:58 -0500

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <body>
+      <div class="" id="chat-input">
+        <form>
+          <input type="text" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </body>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
